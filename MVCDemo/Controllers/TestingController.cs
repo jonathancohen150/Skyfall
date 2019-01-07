2ae08a6a8a757737ccec484735b98d2b2c3be633
Jonathan Cohen <jonathan.cohen@gopaybox.com>
2019-01-07 08:00:12 -0500

    First test

---
+++ MVCDemo/Controllers/TestingController.cs
@@ -0,0 +1,32 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class TestingController : Controller
+    {
+        public object Test { get; set; }
+
+        // GET: Testing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ListTesting()
+        { 
+
+
+            Test.Add(new TestingModel { FirstName = "Cap", LastName = "Ducky", Age = 25 });
+            Test.Add(new TestingModel { FirstName = "Lyrid", LastName = "Doggy", Age = 22 });
+            Test.Add(new TestingModel { FirstName = "Smoke", LastName = "Cat", Age = 27 });
+
+            return View(Test);
+        }
+    }
+}
---
+++ MVCDemo/Models/TestingModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class TestingModel
+    {
+       public string FirstName { get; set; }
+
+       public string LastName { get; set; }
+
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+
+
+    }
+}
